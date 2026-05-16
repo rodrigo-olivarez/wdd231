35d3e52e714bf1556aae69623153f3be09c50dc9
rodrigo-olivarez <olivarezkronnoz@gmail.com>
2026-05-16 10:45:43 -0600

    Continue the page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -12,3 +12,39 @@ navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
 });
+
+async function loadMembers() {
+    const response = await fetch('data/members.json');
+    const members = await response.json();
+    displayMembers(members);
+    document.getElementById('members-container').classList.add('grid');
+}
+loadMembers();
+
+function displayMembers(members) {
+    const container = document.getElementById('members-container');
+    container.innerHTML = "";
+
+    members.forEach(member => {
+        const card = document.createElement('div');
+        card.classList.add('member-card');
+        card.innerHTML = `
+        <img src="images/${member.image}" alt="${member.name}">
+        <h2>${member.name}</h2>
+        <p>${member.address}</p>
+        <p>${member.phone}</p>
+        <a href="${member.website}" target="_blank">${member.website}</a>
+        `;
+        container.appendChild(card);
+    });
+}
+
+document.getElementById('grid-btn').addEventListener('click', () => {
+    document.getElementById('members-container').classList.remove('list');
+    document.getElementById('members-container').classList.add('grid');
+})
+
+document.getElementById('list-btn').addEventListener('click', () => {
+    document.getElementById('members-container').classList.remove('grid');
+    document.getElementById('members-container').classList.add('list');
+})
