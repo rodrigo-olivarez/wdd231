df2bd16c440b47be409e2651748a2ea1f967b100
rodrigo-olivarez <olivarezkronnoz@gmail.com>
2026-06-02 20:09:07 -0600

    Update discover page and integrate places data for enhanced user experience

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,1 +1,31 @@
-import { places } from "../data/places.mjs";
+import { places } from '../data/places.mjs';
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+const modifiedText = `Last Modification: ${document.lastModified}`;
+document.getElementById("lastModified").textContent = modifiedText;
+
+const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+const container = document.getElementById('cards-container');
+places.forEach(place => {
+    const card = document.createElement('section');
+    card.innerHTML = `
+        <h2>${place.name}</h2>
+        <figure>
+            <img src="images/${place.image}" alt="${place.name}" loading="lazy" width="300" height="200">
+        </figure>
+        <address>${place.address}</address>
+        <p>${place.description}</p>
+        <button>Learn More</button>
+    `;
+    container.appendChild(card);
+});
